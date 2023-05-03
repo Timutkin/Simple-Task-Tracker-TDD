f36d8913f617efb42a44559dbb043de0125fb93c
Utkin <xxwild.duckxx@yandex.ru>
2023-05-03 21:47:47 +0300

    A test base and a "test" profile have been added

---
+++ src/test/java/ru/timutkin/tdd/config/PostgresTestProfileConfig.java
@@ -0,0 +1,39 @@
+package ru.timutkin.tdd.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Profile("test")
+public class PostgresTestProfileConfig {
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+
+    @Bean
+    @Profile("test")
+    public DataSource dataSource() {
+        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        return dataSource;
+    }
+}
